3026a47988ce3d16710543f6739c0a1ce98f9d58
MLauraSal <valenarte@gmail.com>
2024-07-04 19:49:40 -0300

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   css/index.css
    	modified:   css/styles.css
    	modified:   index.html
    	new file:   js/login.js

---
+++ js/login.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+/*
+ * FUNCTION LOGIN
+*/
+const loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>
+{
+    loginForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    
+
+}
+
+/* FUNCTION NAVBAR
+*/
+const navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>
+{
+    navbar.classList.toggle('active');
+    loginForm.classList.remove('active');
+    
+}
+
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+    
+}
